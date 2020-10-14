ee2b521029a0e29ecf40861979a0fbfe83c987e4
julian3493 <julianramosarevalo@gmail.com>
2020-10-14 15:14:35 -0500

    change until loop for while loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,15 @@
 # function that takes the array bubble_sort
 def bubble_sort(arr)
   n = arr.length
-  swapped = true
+  no_swapped = true
 
   # this loop code runs until there is nothing to swap
-  until !swapped
-    swapped = false
+  while no_swapped
+    no_swapped = false
     (1...n).each do |i|
       if arr[i - 1] > arr[i]
         arr[i - 1], arr[i] = arr[i], arr[i - 1]
-        swapped = true
+        no_swapped = true
       end
     end
   end
@@ -19,16 +19,16 @@ end
 
 def bubble_sort_by(arr)
   n = arr.length
-  swapped = true
+  no_swapped = true
 
   # swapp loop
-  until !swapped
-    swapped = false
+  while no_swapped
+    no_swapped = false
     (1...n).each do |i|
       # compare the length of the strings into the array
       if arr[i - 1].length > arr[i].length
         arr[i - 1], arr[i] = arr[i], arr[i - 1]
-        swapped = true
+        no_swapped = true
       end
     end
   end
