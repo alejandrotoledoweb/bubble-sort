5cbfc04d04c50a73793c4cce336f426545fb355d
julian3493 <julianramosarevalo@gmail.com>
2020-10-14 14:41:36 -0500

    create bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,5 +17,24 @@ def bubble_sort(arr)
   p arr
 end
 
+def bubble_sort_by(arr)
+  n = arr.length
+  swapped = true
 
+  #swapp loop
+  until !swapped
+    swapped = false
+    (1...n).each do |i|
+      #compare the length of the strings into the array
+      if arr[i-1].length > arr[i].length
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+        swapped = true
+      end
+    end
+  end
+  arr
+  p arr
+end
 
+bubble_sort([4,3,78,2,0,2])
+bubble_sort_by(["hi","hello","hey"])
