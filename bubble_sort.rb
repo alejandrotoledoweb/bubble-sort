05ddff8f2baeb5e778253246762c2608aec0b778
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-14 15:08:11 -0500

    fixed linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,14 @@
-# function that takes the array bubble_sort()
+# function that takes the array bubble_sort
 def bubble_sort(arr)
   n = arr.length
   swapped = true
 
-  #this loop code runs until there is nothing to swap
+  # this loop code runs until there is nothing to swap
   until !swapped
     swapped = false
     (1...n).each do |i|
-      if arr[i-1] > arr[i]
-        arr[i-1], arr[i] = arr[i], arr[i-1]
+      if arr[i - 1] > arr[i]
+        arr[i - 1], arr[i] = arr[i], arr[i - 1]
         swapped = true
       end
     end
@@ -21,13 +21,13 @@ def bubble_sort_by(arr)
   n = arr.length
   swapped = true
 
-  #swapp loop
+  # swapp loop
   until !swapped
     swapped = false
     (1...n).each do |i|
-      #compare the length of the strings into the array
-      if arr[i-1].length > arr[i].length
-        arr[i-1], arr[i] = arr[i], arr[i-1]
+      # compare the length of the strings into the array
+      if arr[i - 1].length > arr[i].length
+        arr[i - 1], arr[i] = arr[i], arr[i - 1]
         swapped = true
       end
     end
@@ -36,5 +36,5 @@ def bubble_sort_by(arr)
   p arr
 end
 
-bubble_sort([4,3,78,2,0,2])
-bubble_sort_by(["hi","hello","hey"])
+bubble_sort([4, 3, 78, 2, 0, 2])
+bubble_sort_by(["hi", "hello", "hey"])
